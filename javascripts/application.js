f474b554148307829c04e2f68426edc37d048ed6
Arnelle Balane <arnellebalane@gmail.com>
2014-04-02 11:13:31 +0800

    reverted to showing only the current state instead of the whole path

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,50 +1,30 @@
-var regex = '(a+b)*ababa';
+var regex = '(a+b)*+aba(ab+ab*+a)*';
 var nfa = RegexParser.parse(regex); 
-
-var a = nfa.getState('q0');
-a.transition(a, 'a');
-
 NFAVisualizer.visualize('#nfa', nfa);
 
-var events = [];
+var states = [];
 nfa.addEventListener('yield', function(e) {
-  events.push(e);
+  states.push(e.state);
 });
 
-console.log(nfa.accepts('ababaa'));
+var regex = 'ababaa';
+console.log(nfa.accepts(regex));
 
-var previous = null;
 step();
 function step() {
-  if (events.length) {
-    var current = document.querySelector('circle.current');
-    if (current) {
-      previous = events.shift().state;
-      var label = document.querySelector('.labels p.current');
-      var transition = document.querySelectorAll('path.current');
-      current.classList.remove('current');
-      label.classList.remove('current');
-      if (transition) {
-        for (var i = 0; i < transition.length; i++) {
-          transition[i].classList.remove('current');
-        }
-      }
+  if (states.length) {
+    var currentState = document.querySelector('circle.current');
+    var currentLabel = document.querySelector('.labels p.current');
+    if (currentState) {
+      currentState.classList.remove('current');
+      currentLabel.classList.remove('current');
     }
-    var state = events.shift().state;
-    var label = document.querySelector('.labels p[for="' + state.label + '"]');
-    state = document.querySelector('circle[label="' + state.label + '"]');
+
+    var label = states.shift().label;
+    var state = document.querySelector('circle[label="' + label + '"]');
+    label = document.querySelector('.labels p[for="' + label + '"]');
     state.classList.add('current');
     label.classList.add('current');
-    if (previous) {
-      var source = previous.label;
-      var destination = state.getAttribute('label');
-      var transition = document.querySelectorAll('path[source="' + source + '"][destination="' + destination + '"]');
-      if (transition) {
-        for (var i = 0; i < transition.length; i++) {
-          transition[i].classList.add('current');
-        }
-      }
-    }
-    setTimeout(step, 500);
+    setTimeout(step, 1000);
   }
 }
